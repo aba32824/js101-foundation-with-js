2262a0e9163f9ba6c560f530825a5ee8513e3ceb
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-03 01:22:22 +0300

    Update isOdd.js
    Function `isOdd` implemented

--- SmallProblems/Easy1/isOdd.js
+++ SmallProblems/Easy1/isOdd.js
@@ -1,5 +1,11 @@
-// To test whether a number is odd or even
+// To test whether a number is an odd or even
 
 const isOdd = (num) => {
-  return false;
+  return Boolean(Math.abs(num) % 2);
 };
+
+// to run a basic test
+let numbers = [2, 5, -17, -8, 0, 7];
+for (let n of numbers) {
+  console.log(`Number ${n} is odd? ${isOdd(n)}`);
+}
