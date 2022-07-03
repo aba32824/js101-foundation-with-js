1c2a3e176199b9c3ea0033b8ab910835dddd7d64
aba32824@slack.launchschool.com <okikukai.kiev@gmail.com>
2022-07-03 22:06:41 +0300

    Solution for the 2nd task
    Solution added + updated previous script

--- SmallProblems/Easy1/isOdd.js
+++ SmallProblems/Easy1/isOdd.js
@@ -4,8 +4,4 @@ const isOdd = (num) => {
   return Boolean(Math.abs(num) % 2);
 };
 
-// to run a basic test
-let numbers = [2, 5, -17, -8, 0, 7];
-for (let n of numbers) {
-  console.log(`Number ${n} is odd? ${isOdd(n)}`);
-}
+module.exports = { isOdd };
